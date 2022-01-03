a5be82a6ce4199c9c1ce6ce75c74c1873dfa5bf4
Rhenato Francisco Baraúna <rhbarauna@gmail.com>
2022-01-03 08:36:39 -0300

    Readme update + global exception handler

---
+++ src/main/java/com/barauna/DEVinHouse/controller/ControllersExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.barauna.DEVinHouse.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllersExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> errorHandler(Exception ex, WebRequest request) {
+
+        return handleExceptionInternal(ex, ex.getMessage(),
+                new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY, request);
+    }
+}
